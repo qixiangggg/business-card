a8571eb05968643fee87cfe3a8ff27bd6038c86b
desmond.chong <desmond.chong@beans.com.my>
2025-11-13 21:41:05 +0800

    Create info section

---
+++ business-card/src/components/Info.jsx
@@ -0,0 +1,13 @@
+import avatar from "../assets/avatar.png"
+export default function Info(){
+    return (
+        <header>
+            <img src={avatar}/>
+            <h1>Desmond Chong Qi Xiang</h1>
+            <h2>Full Stack Developer</h2>
+            <p>desmondchong.webste</p>
+            <a href="mailto:qixiangggg@gmail.com">Email</a>
+            <a href="https://www.linkedin.com/in/desmond-chong-qi-xiang-7098a0235/">LinkedIn</a>
+        </header>
+    )
+}
