8bbb5256c076c26fcacae1eb027770c3b07ea94a
desmond.chong <desmond.chong@beans.com.my>
2025-11-14 14:33:26 +0800

    Styling for info, about and interests section

--- business-card/src/components/Info.jsx
+++ business-card/src/components/Info.jsx
@@ -1,13 +1,18 @@
-import avatar from "../assets/avatar.png"
+import emailIcon from "../assets/email-icon.png"
+import linkedinIcon from "../assets/linkedin-icon.png"
 export default function Info(){
     return (
-        <header>
-            <img src={avatar}/>
-            <h1>Desmond Chong Qi Xiang</h1>
-            <h2>Full Stack Developer</h2>
-            <p>desmondchong.webste</p>
-            <a href="mailto:qixiangggg@gmail.com">Email</a>
-            <a href="https://www.linkedin.com/in/desmond-chong-qi-xiang-7098a0235/">LinkedIn</a>
+        <header className="header">
+            <div className="personal-photo" aria-label="personal-photo"></div>
+            <div className="container">
+                <h1 className="name">Desmond Chong Qi Xiang</h1>
+                <h2 className="profession">Full Stack Developer</h2>
+                <p className="website">desmondchong.webste</p>
+                <div className="link">
+                    <a href="mailto:qixiangggg@gmail.com" className="email"><img src={emailIcon} className="email-icon"/>Email</a>
+                    <a href="https://www.linkedin.com/in/desmond-chong-qi-xiang-7098a0235/" className="linkedin" ><img src={linkedinIcon} className="linkedin-icon"/>LinkedIn</a>
+                </div>
+            </div>
         </header>
     )
 }
--- business-card/src/components/Interests.jsx
+++ business-card/src/components/Interests.jsx
@@ -1,11 +1,11 @@
 export default function Interests(){
     return(
-        <>
+        <section className="container">
             <h3>Interests</h3>
-            <p>
+            <p className="paragraph">
                 Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. 
                 Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
             </p>
-        </>
+        </section>
     )
 }
